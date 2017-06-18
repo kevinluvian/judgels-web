d478de80331ea4b46db578e053602824fad91e9f
Kevin Luvian <senaidert@gmail.com>
2017-06-18 12:40:20 +0700

    move carouselcontent to different component

--- src/routes/Home/components/HomeView/HomeView.js
+++ src/routes/Home/components/HomeView/HomeView.js
@@ -1,31 +1,8 @@
 import React from 'react'
-import { Container, Divider, Grid, Header, Label, Button, Segment, Image } from 'semantic-ui-react'
+import { Container, Divider, Grid, Header, Label, Button, Segment } from 'semantic-ui-react'
+import CarouselContent from '../CarouselContent'
 import Slider from 'react-slick'
 import '../../styles/HomeView.css'
-
-const CarouselContent = ({ header, subheader, headerContent, image }) => (
-  <Grid style={{ minHeight: '40vh', marginLeft: '60px', marginRight: '60px' }} stackable>
-    <Grid.Column width={12} verticalAlign='middle'>
-      <Header size='huge' style={{ fontWeight: 100 }}>
-        {header}
-        <Header.Subheader style={{ fontSize: '1rem' }}>
-          {subheader}
-        </Header.Subheader>
-      </Header>
-      {headerContent}
-    </Grid.Column>
-    <Grid.Column width={4} verticalAlign='middle'>
-      <Image src={image} />
-    </Grid.Column>
-  </Grid>
-)
-
-CarouselContent.propTypes = {
-  header: React.PropTypes.string,
-  subheader: React.PropTypes.string,
-  headerContent: React.PropTypes.element,
-  image: React.PropTypes.string
-}
 
 const HomeView = () => {
   var settings = {
